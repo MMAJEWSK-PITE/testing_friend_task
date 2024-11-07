09338c302a72304c0f65e5097b3f147b92f07fd4
mwmajewsk <5279578+mmajewsk@users.noreply.github.com>
2024-11-07 20:36:46 +0100

    Create task.py

---
+++ task.py
@@ -0,0 +1,11 @@
+# Your task is to get the code from your friend
+# copy the code of your friend, preferably as separate files to this repository
+# Test all of the methods in their code, or at least 10 unittest tests (that make sense).
+# Your test must be runnable with the command `python -m unittest task`
+# Write down the address of your friends repo here:
+# 
+# Imported from: ADRESS_LINK_HERE
+#
+
+# change the name of OTHER_CODE, to the code that you imported
+import OTHER_CODE
